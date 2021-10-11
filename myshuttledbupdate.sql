d496ecfaec4b0dba64c7ef6a41bd7f331c18dce6
jethanivijay <66484052+jethanivijay@users.noreply.github.com>
2021-10-11 13:20:39 +0530

    Update myshuttledbupdate.sql

--- myshuttledbupdate.sql
+++ myshuttledbupdate.sql
@@ -1,3 +1,3 @@
 use alm;
-INSERT INTO `alm`.`employees` (`id`, `username`, `password`) VALUES ('22', 'vijay', 'vijaypassword');
-INSERT INTO `alm`.`fares` (`id`, `emp_id`, `pickup`, `dropoff`, `start`, `end`, `fare_charge`, `driver_fee`, `passenger_rating`, `driver_rating`) VALUES ('31', '22', '43 Brontosaurus Blvd, Bedrock, WA', '51 Stegasaurus St, Rock Gardens, WA', '2021-10-12 11:41:00', '2021-10-12 12:41:00', '1000', '800', '4', '5');
+INSERT INTO `alm`.`employees` (`id`, `username`, `password`) VALUES ('23', 'vijay1', 'vijaypassword');
+INSERT INTO `alm`.`fares` (`id`, `emp_id`, `pickup`, `dropoff`, `start`, `end`, `fare_charge`, `driver_fee`, `passenger_rating`, `driver_rating`) VALUES ('32', '23', '43 Brontosaurus Blvd, Bedrock, WA', '51 Stegasaurus St, Rock Gardens, WA', '2021-10-12 11:41:00', '2021-10-12 12:41:00', '1000', '800', '4', '5');
