53399cf7e7c21b6c6164893f156875475b5b84b2
jethanivijay <66484052+jethanivijay@users.noreply.github.com>
2021-11-29 22:03:03 +0530

    Update CosmosClientFactory.java

--- src/com/microsoft/azure/cosmos/sample/dao/CosmosClientFactory.java
+++ src/com/microsoft/azure/cosmos/sample/dao/CosmosClientFactory.java
@@ -6,7 +6,7 @@ import com.azure.cosmos.CosmosClientBuilder;
 
 public class CosmosClientFactory {
     private static final String HOST = "https://vijayjavacosmoshol.documents.azure.com:443/";
-    private static final String MASTER_KEY = "inJVnKY1f3iYgiyxWJXkgiy3vn2yoxIACVmlSWBoGpEzov9TDVrZogsyVZ9uYqNx7KTYTCyuRMslaElENugpxw==";
+    private static final String MASTER_KEY = "EYpLOHH3pRqROlzwu4lsvo659tzYSeUjBSma0OWkaEzezFZ1HIcxgX8HT4CMijoaRNRFM5T4dU7sA2JoYgZedw==";
 
     private static CosmosClient cosmosClient = new CosmosClientBuilder()
             .endpoint(HOST)
